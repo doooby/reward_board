6d8a0c82a737bc6577e7f3a514e8b820143202c4
d3oby <zelazk.o@email.cz>
2021-04-16 21:26:50 +0200

    moved server

--- demo/lib/server.js
+++ demo/lib/server.js
@@ -4,16 +4,16 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('views', path.resolve(__dirname, './views'));
+app.set('views', path.resolve(__dirname, '../views'));
 app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('home_page.html');
 });
 
 app.get('/assets/*', (req, res) => {
-    const file = path.resolve(__dirname, '../tmp/build', req.params[0]);
+    const file = path.resolve(__dirname, '../../tmp/build', req.params[0]);
     if (fs.existsSync(file)) {
         res.sendFile(file);
     } else {
