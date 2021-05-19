5a2ac397732804a0ae1084f06c4a54eba42b3dfd
d3oby <zelazk.o@email.cz>
2021-05-19 18:13:08 +0200

    live demo

--- demo/index.js
+++ demo/index.js
@@ -1,30 +1,29 @@
 window.D3O_RewardBoard(async ({Board, fetch}) => {
-    const apiBaseUrl = 'http://localhost:3000/api';
+    const apiBaseUrl = 'http://localhost:3000';
 
     // pri nacteni stranky je potreba ziskat data odmen:
-    // const rewards = await fetch(apiBaseUrl + '/rewards', {method: 'GET'});
-    // if (rewards === null) throw new Error('rewards fetch failed');
-    const rewards = [
-        { x: 3, y: -3, style: { color: 'red' }, reward_id: 1 },
-        { x: 6, y: -6, style: { color: 'red' }, reward_id: 2 },
-    ];
+    const rewards = await fetch(apiBaseUrl + '/rewards', { method: 'GET' });
+    if (rewards === null) throw new Error('rewards fetch failed');
+    // const rewards = [
+    //     { x: 3, y: -3, style: { color: 'red' }, reward_id: 1 },
+    //     { x: 6, y: -6, style: { color: 'red' }, reward_id: 2 },
+    // ];
 
     // take nacist aktualni pozici uzivatele:
     // x+ = vychod, x- = zapad, y+ = sever, y- = jih
-    // const playerData = await fetch(apiBaseUrl + '/player', {method: 'GET'});
-    // if (playerData === null) throw new Error('playerData fetch failed');
-    const playerData = {
-        moves: 10,
-        position: { x: 0, y: 0 },
-    };
+    const defaultPlayerData = await fetch(apiBaseUrl + '/player', { method: 'GET' });
+    if (defaultPlayerData === null) throw new Error('defaultPlayerData fetch failed');
+    // const defaultPlayerData = {
+    //     moves: 10,
+    //     position: { x: 0, y: 0 },
+    // };
 
     // udalost po stistku tlacitka pohybu
     async function onStepRequested (position) {
         // aby se ignorovalo dalsi kliknuti nez se to vyhodnoti
         buttons.disableAll();
 
-        // const result = await fetch(apiBaseUrl + '/move', position);
-        const result = { valid: true, moves: 9};
+        const result = await fetch(apiBaseUrl + '/move', { data: position });
         if (result === null || !result.valid) {
             // tah neni validni nebo neco selhalo v komunikaci se serverem
             // zobrazit nejakou informaci uzivateli?
@@ -55,14 +54,14 @@ window.D3O_RewardBoard(async ({Board, fetch}) => {
     const buttons = Board.findButtons(direction => document.getElementById(`rewards-btn-${direction}`));
     const board = new Board({
         element: document.querySelector('#board-container'),
-        defaultPosition: playerData.position,
+        defaultPosition: defaultPlayerData.position,
         rewards,
         onStepRequested,
         onPositionClick,
         onMouseOverPosition,
     });
     buttons.onClick = direction => board.step(direction);
-    if (playerData.moves > 0) buttons.update(board);
+    if (defaultPlayerData.moves > 0) buttons.update(board);
 
     window.D3O_REWARDS_BOARD = board;
 });
